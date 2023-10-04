0760d0e8fc24a43f8c29106c489c5b87ce2fe295
SathyaS5 <Sathyalakshmanan345@gmail.com>
2023-10-04 16:17:05 +0530

    project code

--- src/test/java/org/github/Excelgithub.java
+++ src/test/java/org/github/Excelgithub.java
@@ -1,5 +1,19 @@
 package org.github;
 
-public class Excelgithub {
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
+public class Excelgithub {
+	@Test
+	public void Amazon() {
+	WebDriverManager.edgedriver().setup();
+	WebDriver driver = new EdgeDriver();
+	driver.get("https://www.amazon.in/log-in/s?k=log+in");
+
+	
+	}
 }
